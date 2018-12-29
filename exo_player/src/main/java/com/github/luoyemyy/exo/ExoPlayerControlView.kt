3e74f60095c1c95c1275700db9b97407617f08aa
yeyuming <luoyemyy@qq.com>
2018-12-29 09:59:49 +0800

    update

--- exo_player/src/main/java/com/github/luoyemyy/exo/ExoPlayerControlView.kt
+++ exo_player/src/main/java/com/github/luoyemyy/exo/ExoPlayerControlView.kt
@@ -2,10 +2,84 @@ package com.github.luoyemyy.exo
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ImageView
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ControlDispatcher
+import com.google.android.exoplayer2.DefaultControlDispatcher
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.PlayerControlView
 
 class ExoPlayerControlView constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : PlayerControlView(context, attributeSet, defStyleAttr) {
+
     constructor(context: Context, attributeSet: AttributeSet?) : this(context, attributeSet, 0)
 
     constructor(context: Context) : this(context, null, 0)
+
+    private lateinit var mImgBack: ImageView
+    private lateinit var mImgPlay: ImageView
+    private lateinit var mImgPause: ImageView
+    private lateinit var mImgFullscreen: ImageView
+    private lateinit var mLayout: View
+
+
+    private lateinit var mControlDispatcher: ControlDispatcher
+    private var mFullscreen: Boolean = false
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        mImgBack = findViewById(R.id.imgBack)
+        mImgPlay = findViewById(R.id.exo_play)
+        mImgPause = findViewById(R.id.exo_pause)
+        mImgFullscreen = findViewById(R.id.imgFullscreen)
+        mLayout = findViewById(R.id.controlLayout)
+        mImgBack.setOnClickListener(clickListener)
+        mImgPlay.setOnClickListener(clickListener)
+        mImgPause.setOnClickListener(clickListener)
+        mImgFullscreen.setOnClickListener(clickListener)
+        mLayout.setOnClickListener(clickListener)
+
+        mControlDispatcher = DefaultControlDispatcher()
+        setControlDispatcher(mControlDispatcher)
+
+        visibility(mImgBack, mFullscreen)
+        visibility(mImgFullscreen, !mFullscreen)
+        mImgFullscreen.setImageResource(if (mFullscreen) R.drawable.exo_controls_fullscreen_exit else R.drawable.exo_controls_fullscreen_enter)
+
+        post {
+            visibility(mImgPlay, true)
+        }
+    }
+
+    private fun visibility(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+
+    private val clickListener = OnClickListener { v ->
+        when (v) {
+            mImgBack -> {
+                ExoPlayerHelper.getInstance().exitFullScreen(true)
+            }
+            mImgPause -> {
+                mControlDispatcher.dispatchSetPlayWhenReady(player, false)
+            }
+            mImgPlay -> {
+                if (player.playbackState == Player.STATE_ENDED) {
+                    mControlDispatcher.dispatchSeekTo(player, player.currentWindowIndex, C.TIME_UNSET)
+                }
+                mControlDispatcher.dispatchSetPlayWhenReady(player, true)
+            }
+            mImgFullscreen -> {
+                ExoPlayerHelper.getInstance().enterFullscreen(context)
+            }
+            mLayout -> {
+                show()
+            }
+        }
+    }
+
 }
--- exo_player/src/main/java/com/github/luoyemyy/exo/ExoPlayerHelper.kt
+++ exo_player/src/main/java/com/github/luoyemyy/exo/ExoPlayerHelper.kt
@@ -1,34 +1,79 @@
 package com.github.luoyemyy.exo
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.ConnectivityManager
+import android.net.Uri
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 
 
-class ExoPlayerHelper private constructor(){
+class ExoPlayerHelper private constructor() {
 
     companion object {
 
-        const val NETWORK_EVENT = ""
+//        const val NETWORK_EVENT = ""
 
-    }
-
-    val networkReceiver= object :BroadcastReceiver(){
-        override fun onReceive(context: Context?, intent: Intent?) {
-
+        fun getInstance(): ExoPlayerHelper {
+            return ExoPlayerHelper()
         }
     }
 
-    fun s(context: Context){
-        context.registerReceiver(networkReceiver, IntentFilter().apply {
-            addAction(ConnectivityManager.CONNECTIVITY_ACTION)
-        })
+    private var mPlayer: SimpleExoPlayer? = null
+    private var mFullscreenDialog: FullscreenPlayerFragment? = null
+    private var mPlayerView: PlayerView? = null
+
+    fun enterFullscreen(context: Context) {
+        val fm = when (context) {
+            is FragmentActivity -> {
+                context.supportFragmentManager
+            }
+            is Fragment -> {
+                context.fragmentManager
+            }
+            else -> return
+        }
+        mFullscreenDialog = FullscreenPlayerFragment().apply { show(fm, "fullscreenPlayer") }
     }
 
-    fun hasWifi() {
-
+    fun exitFullScreen(dismissDialog: Boolean = true) {
+        if (dismissDialog) {
+            mFullscreenDialog?.dismiss()
+        }
+        mPlayerView?.player = mPlayer
     }
+
+    fun playCurrent(playerView: PlayerView) {
+        mPlayer?.apply {
+            PlayerView.switchTargetView(this, mPlayerView, playerView)
+        }
+    }
+
+    fun play(url: String, playerView: PlayerView) {
+        val context = playerView.context
+        val source = ExtractorMediaSource.Factory(DefaultDataSourceFactory(context, Util.getUserAgent(context, context.packageName))).createMediaSource(Uri.parse(url))
+        mPlayer = ExoPlayerFactory.newSimpleInstance(context).apply { prepare(source) }
+        mPlayerView = playerView
+    }
+
+//    val networkReceiver = object : BroadcastReceiver() {
+//        override fun onReceive(context: Context?, intent: Intent?) {
+//
+//        }
+//    }
+//
+//    fun s(context: Context) {
+//        context.registerReceiver(networkReceiver, IntentFilter().apply {
+//            addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+//        })
+//    }
+//
+//    fun hasWifi() {
+//
+//    }
 
 }
